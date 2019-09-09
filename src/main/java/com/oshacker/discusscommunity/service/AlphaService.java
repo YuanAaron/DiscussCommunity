dc9f316d127073f387d74572d42ed8dd12bd3628
YuanAaron <1500438364@qq.com>
2019-09-09 10:17:32 +0800

    事务管理

--- src/main/java/com/oshacker/discusscommunity/service/AlphaService.java
+++ src/main/java/com/oshacker/discusscommunity/service/AlphaService.java
@@ -1,12 +1,25 @@
 package com.oshacker.discusscommunity.service;
 
 import com.oshacker.discusscommunity.dao.AlphaDao;
+import com.oshacker.discusscommunity.dao.DiscussPostMapper;
+import com.oshacker.discusscommunity.dao.UserMapper;
+import com.oshacker.discusscommunity.entity.DiscussPost;
+import com.oshacker.discusscommunity.entity.User;
+import com.oshacker.discusscommunity.utils.DiscussCommunityUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import java.util.Date;
 
 @Service
 //@Scope("prototype") //不在程序启动时实例化Bean，而是每次getBean都会实例化一个Bean
@@ -14,6 +27,15 @@ public class AlphaService {
 
     @Autowired
     private AlphaDao alphaDao;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private DiscussPostMapper discussPostMapper;
+
+    @Autowired
+    private TransactionTemplate transactionTemplate;
 
     public AlphaService() {
 //        System.out.println("实例化AlphaService");
@@ -33,4 +55,68 @@ public class AlphaService {
         return alphaDao.select();
     }
 
+    //业务方法A调用业务方法B，这两个业务方法都可能加上@Transactional去管理事务，
+    //这样的情况下，B的事务是以B本身的机制为准，还是以A的为准，还是以其他的方式为准呢？
+    //这涉及到两个事务交叉在一起的问题，事务的传播机制解决的就是这种交叉的问题。
+    // REQUIRED: 支持当前事务(外部事务),如果当前事务不存在则创建新事务.
+    // REQUIRES_NEW: 创建一个新事务,并且暂停当前事务(外部事务).
+    // NESTED: 如果当前事务(外部事务)存在,则嵌套在外部事务中执行(事务B能独立的提交和回滚)；
+    // 如果当前事务不存在，那么就和REQUIRED一样.
+
+    //声明式事务
+    @Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED)
+    public Object save1() {
+        // 新增用户
+        User user = new User();
+        user.setUsername("alpha");
+        user.setSalt(DiscussCommunityUtil.generateUUID().substring(0, 5));
+        user.setPassword(DiscussCommunityUtil.md5("123" + user.getSalt()));
+        user.setEmail("alpha@qq.com");
+        user.setHeaderUrl("http://images.nowcoder.com/head/99t.png");
+        user.setCreateTime(new Date());
+        userMapper.insertUser(user);
+
+        // 新增帖子
+        DiscussPost post = new DiscussPost();
+        post.setUserId(user.getId());
+        post.setTitle("Hello");
+        post.setContent("新人报道!");
+        post.setCreateTime(new Date());
+        discussPostMapper.insertDiscussPost(post);
+
+        Integer.valueOf("abc"); //故意报错，让其回滚
+        return "ok";
+    }
+
+    //编程式事务
+    public Object save2() {
+        transactionTemplate.setIsolationLevel(TransactionDefinition.ISOLATION_READ_COMMITTED);
+        transactionTemplate.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+
+        return transactionTemplate.execute(new TransactionCallback<Object>() {
+            @Override
+            public Object doInTransaction(TransactionStatus status) {
+                // 新增用户
+                User user = new User();
+                user.setUsername("beta");
+                user.setSalt(DiscussCommunityUtil.generateUUID().substring(0, 5));
+                user.setPassword(DiscussCommunityUtil.md5("123" + user.getSalt()));
+                user.setEmail("beta@qq.com");
+                user.setHeaderUrl("http://image.nowcoder.com/head/999t.png");
+                user.setCreateTime(new Date());
+                userMapper.insertUser(user);
+
+                // 新增帖子
+                DiscussPost post = new DiscussPost();
+                post.setUserId(user.getId());
+                post.setTitle("你好");
+                post.setContent("我是新人!");
+                post.setCreateTime(new Date());
+                discussPostMapper.insertDiscussPost(post);
+
+                Integer.valueOf("abc");
+                return "ok";
+            }
+        });
+    }
 }
