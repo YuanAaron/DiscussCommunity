d39a07d1b94ea814b953ddc9059a51678b0e34fa
YuanAaron <1500438364@qq.com>
2019-09-17 19:05:12 +0800

    用AOP统一记录日志

---
+++ src/main/java/com/oshacker/discusscommunity/aspect/AlphaAspect.java
@@ -0,0 +1,42 @@
+package com.oshacker.discusscommunity.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+
+//@Component
+//@Aspect
+public class AlphaAspect {
+
+    @Pointcut("execution(* com.oshacker.discusscommunity.service.*.*(..))")
+    public void pointcut() {
+    }
+
+    @Before("pointcut()")
+    public void before() {
+        System.out.println("before");
+    }
+
+    @After("pointcut()")
+    public void after() {
+        System.out.println("after");
+    }
+
+    @AfterReturning("pointcut()")
+    public void afterReturning() {
+        System.out.println("afterReturning");
+    }
+
+    @AfterThrowing("pointcut()")
+    public void afterThrowing() {
+        System.out.println("afterThrowing");
+    }
+
+    @Around("pointcut()")
+    public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
+        System.out.println("around before");
+        Object obj = joinPoint.proceed();
+        System.out.println("around after");
+        return obj;
+    }
+}
