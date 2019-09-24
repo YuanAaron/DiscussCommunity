ba6b0359f5bb81754ac67c50516507998ed0d604
YuanAaron <1500438364@qq.com>
2019-09-24 11:15:12 +0800

    所有的未读消息数

--- src/main/java/com/oshacker/discusscommunity/config/WebMvcConfig.java
+++ src/main/java/com/oshacker/discusscommunity/config/WebMvcConfig.java
@@ -3,6 +3,7 @@ package com.oshacker.discusscommunity.config;
 import com.oshacker.discusscommunity.interceptor.AlphaInterceptor;
 import com.oshacker.discusscommunity.interceptor.LoginRequiredIntercepter;
 import com.oshacker.discusscommunity.interceptor.LoginTicketInterceptor;
+import com.oshacker.discusscommunity.interceptor.MessageIntercepter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -20,6 +21,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Autowired
     private LoginRequiredIntercepter loginRequiredIntercepter;
 
+    @Autowired
+    private MessageIntercepter messageIntercepter;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(alphaInterceptor)
@@ -30,6 +34,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
 
         registry.addInterceptor(loginRequiredIntercepter)
+                .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
+
+        registry.addInterceptor(messageIntercepter)
                 .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
     }
 }
--- src/main/java/com/oshacker/discusscommunity/interceptor/MessageIntercepter.java
+++ src/main/java/com/oshacker/discusscommunity/interceptor/MessageIntercepter.java
@@ -1,4 +1,34 @@
 package com.oshacker.discusscommunity.interceptor;
 
-public class MessageIntercepter {
+import com.oshacker.discusscommunity.entity.User;
+import com.oshacker.discusscommunity.service.MessageService;
+import com.oshacker.discusscommunity.utils.HostHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import sun.misc.resources.Messages;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class MessageIntercepter implements HandlerInterceptor {
+
+    @Autowired
+    private HostHolder hostHolder;
+
+    @Autowired
+    private MessageService messageService;
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) {
+        User user = hostHolder.getUser();
+        if (user!=null&&modelAndView!=null) {
+            int letterUnreadCount = messageService.findLetterUnreadCount(user.getId(), null);
+            int noticeUnreadCount = messageService.findNoticeUnreadCount(user.getId(), null);
+            modelAndView.addObject("allUnreadCount",letterUnreadCount+noticeUnreadCount);
+        }
+
+    }
 }
